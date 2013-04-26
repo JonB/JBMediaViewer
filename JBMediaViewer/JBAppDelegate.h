347848200e81b7a5664deb7c67e539df1ba47435
Jonathan Blower <Jon@theblowers.org.uk>
2013-04-26 07:17:56 +0100

    Initial Commit

---
+++ JBMediaViewer/JBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JBAppDelegate.h
+//  JBMediaViewer
+//
+//  Created by Jonathan Blower on 26/04/2013.
+//  Copyright (c) 2013 Jon Blower. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JBMediaViewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JBMediaViewer
+//
+//  Created by Jonathan Blower on 26/04/2013.
+//  Copyright (c) 2013 Jon Blower. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JBAppDelegate class]));
+    }
+}
